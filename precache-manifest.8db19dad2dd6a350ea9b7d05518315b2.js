6c940c19c95bba20df91349b18327c5999a7c871
drchanvit <chanvitt@gmail.com>
2020-09-16 18:06:34 +0700

    Updates

---
+++ precache-manifest.8db19dad2dd6a350ea9b7d05518315b2.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c878a8cd551fd1f82513a735b65d8ab7",
+    "url": "/r8cs/index.html"
+  },
+  {
+    "revision": "d28460f5a3cef1403c8c",
+    "url": "/r8cs/static/js/0.a77ea0de.chunk.js"
+  },
+  {
+    "revision": "aaf66991fe8009887054",
+    "url": "/r8cs/static/js/1.4136b3b1.chunk.js"
+  },
+  {
+    "revision": "9784fe453d3b67bff0a2",
+    "url": "/r8cs/static/js/10.b6314479.chunk.js"
+  },
+  {
+    "revision": "883fc2bf76f444986348",
+    "url": "/r8cs/static/js/11.c3c187ba.chunk.js"
+  },
+  {
+    "revision": "8489650fee83b07427af",
+    "url": "/r8cs/static/js/12.f5287ec5.chunk.js"
+  },
+  {
+    "revision": "5eb50d8d014b23d8c57c",
+    "url": "/r8cs/static/js/13.141255ed.chunk.js"
+  },
+  {
+    "revision": "06658f84ffbfa66474cd",
+    "url": "/r8cs/static/js/14.dc0330b6.chunk.js"
+  },
+  {
+    "revision": "d43158c5980792c62e7d",
+    "url": "/r8cs/static/js/15.a8a13573.chunk.js"
+  },
+  {
+    "revision": "226361f38ceabcd7642d",
+    "url": "/r8cs/static/js/4.4083db49.chunk.js"
+  },
+  {
+    "revision": "bb07bf585b5364cfbb4632dd9308d60e",
+    "url": "/r8cs/static/js/4.4083db49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7828276fa6c4fde1b8ae",
+    "url": "/r8cs/static/js/5.2d1286fd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/r8cs/static/js/5.2d1286fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f74e38541cfd072577ee",
+    "url": "/r8cs/static/js/6.dc8681fc.chunk.js"
+  },
+  {
+    "revision": "c2f9382b7e08733ca459",
+    "url": "/r8cs/static/js/7.56886d16.chunk.js"
+  },
+  {
+    "revision": "a2e07a025696d8f09601",
+    "url": "/r8cs/static/js/8.a4f04805.chunk.js"
+  },
+  {
+    "revision": "41159c4b4eb9d9924289",
+    "url": "/r8cs/static/js/9.8bbd9f8c.chunk.js"
+  },
+  {
+    "revision": "6f9c0320579fd6f1f63e",
+    "url": "/r8cs/static/js/main.ad4d4c49.chunk.js"
+  },
+  {
+    "revision": "9eedbf016790bf083ff9",
+    "url": "/r8cs/static/js/runtime-main.4fa9f86c.js"
+  },
+  {
+    "revision": "f6fc0f8a81c8d82b49a4452acf8d813b",
+    "url": "/r8cs/static/media/childShield1.f6fc0f8a.png"
+  }
+]);
