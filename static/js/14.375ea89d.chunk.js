a35309c6e94b9782e64f8c931f9d1f1212e36d62
drchanvit <chanvitt@gmail.com>
2020-09-16 13:39:10 +0700

    Updates

--- static/js/14.375ea89d.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpr8cs=this.webpackJsonpr8cs||[]).push([[14],{617:function(s,t,e){"use strict";e.r(t);var n=e(0),c=e.n(n),r=e(11);t.default=function(){var s=Object(r.e)().slug;return c.a.createElement("div",null,"Now showing post ",s)}}}]);
-//# sourceMappingURL=14.375ea89d.chunk.js.map
